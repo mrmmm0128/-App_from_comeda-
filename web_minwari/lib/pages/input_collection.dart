eeed1c1da798b54d28cee3e2dba459865492b975
souta21 <soutaginta@gmail.com>
2024-11-16 01:50:56 +0900

    add help button

---
+++ web_minwari/lib/pages/input_collection.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:study_flutter_firebase/pages/add_memo_page.dart';
+import 'package:study_flutter_firebase/pages/top_page.dart';
+
+class CollectionInputPage extends StatefulWidget {
+  const CollectionInputPage({super.key});
+
+  @override
+  _CollectionInputPageState createState() => _CollectionInputPageState();
+}
+
+class _CollectionInputPageState extends State<CollectionInputPage> {
+  final TextEditingController _controller = TextEditingController();
+  final TextEditingController _newGroupController = TextEditingController();
+
+  // Firestoreに新しいグループがすでに存在するかを確認する非同期関数
+  Future<bool> _checkIfGroupExists(String groupName) async {
+    try {
+      final snapshot =
+          await FirebaseFirestore.instance.collection(groupName).get();
+      return snapshot.docs.isNotEmpty; // ドキュメントがあれば、グループ名は既に存在
+    } catch (e) {
+      print("Error checking group: $e");
+      return false;
+    }
+  }
+
+  // 既存グループに移動
+  void _navigateToNextPage(String collectionName) async {
+    if (collectionName.isNotEmpty) {
+      // グループが存在するか確認
+      bool groupExists = await _checkIfGroupExists(collectionName);
+
+      if (groupExists) {
+        // グループが存在すればページ遷移
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MyHomePage(collectionName: collectionName),
+          ),
+        );
+      } else {
+        // グループが存在しない場合、エラーメッセージを表示
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('指定されたグループは存在しません')),
+        );
+      }
+    } else {
+      // グループ名が空の場合、エラーメッセージを表示
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('グループ名を入力してください')),
+      );
+    }
+  }
+
+  // 新しいグループを作成
+  void _createNewGroupAndNavigate(String groupName) async {
+    if (groupName.isNotEmpty) {
+      bool groupExists = await _checkIfGroupExists(groupName);
+
+      if (!groupExists) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => AddMemoPage(collectionName: groupName),
+          ),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('新しいグループが作成されました')),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('このグループ名はすでに使われています')),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('新しいグループ名を入力してください')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "グループ名入力",
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 22,
+          ),
+        ),
+        backgroundColor: const Color(0xFF75A9D6), //Appbarの色,
+        foregroundColor: Colors.white,
+        elevation: 4,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: Text(
+                'グループを作って仲間との支払記録を共有しましょう!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto',
+                  color: Colors.blueGrey,
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'グループ名',
+                labelStyle: TextStyle(color: Colors.blueGrey),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue.shade400),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _navigateToNextPage(_controller.text),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF75A9D6),
+                foregroundColor: Colors.white,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: const Text(
+                '既存グループに移動',
+                style: TextStyle(fontFamily: "Roboto", fontSize: 16),
+              ),
+            ),
+            const Divider(
+              height: 40,
+              color: Colors.blueGrey,
+              thickness: 1.0,
+            ),
+            TextField(
+              controller: _newGroupController,
+              decoration: InputDecoration(
+                labelText: '新しいグループ名',
+                labelStyle: TextStyle(color: Colors.blueGrey),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue.shade400),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () =>
+                  _createNewGroupAndNavigate(_newGroupController.text),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF75A9D6),
+                foregroundColor: Colors.white,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: const Text(
+                '新しいグループを作成',
+                style: TextStyle(fontFamily: "Roboto", fontSize: 16),
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: const Color(0xFFE0ECF8), // 背景色,
+    );
+  }
+}
---
+++ web_minwari/lib/pages/top_page.dart
@@ -0,0 +1,245 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:study_flutter_firebase/model/memo.dart';
+import 'package:study_flutter_firebase/pages/add_memo_page.dart';
+import 'package:study_flutter_firebase/pages/memo_detail_page.dart';
+import 'package:study_flutter_firebase/pages/input_collection.dart';
+import 'package:study_flutter_firebase/pages/explain.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.collectionName});
+
+  final String collectionName;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late CollectionReference memoCollection;
+
+  @override
+  void initState() {
+    super.initState();
+    memoCollection =
+        FirebaseFirestore.instance.collection(widget.collectionName);
+  }
+
+  void _deleteMemo(String id) async {
+    await memoCollection.doc(id).delete();
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('メモを削除しました')),
+    );
+  }
+
+  void _navigateToExplain(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const Explain()),
+    );
+  }
+
+  void _getLink(String memoId, String collectionName) async {
+    // メモIDをURLエンコードしてリンクを生成
+    String link = "https://groupwarikan.com/travel/$collectionName/$memoId";
+
+    // クリップボードにリンクをコピー
+    Clipboard.setData(ClipboardData(text: link)).then((_) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("リンクがクリップボードにコピーされました")),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "みんなで割り勘",
+          style: TextStyle(
+            fontFamily: 'Roboto',
+          ),
+        ),
+        backgroundColor: const Color(0xFF75A9D6), //Appbarの色
+        foregroundColor: Colors.white,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.help_outline), // ボタンのアイコン
+            onPressed: () => _navigateToExplain(context), // ページ遷移
+          ),
+        ],
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: memoCollection.snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (!snapshot.hasData) {
+            return const Center(child: Text("データがありません"));
+          }
+          final docs = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              Map<String, dynamic> data =
+                  docs[index].data() as Map<String, dynamic>;
+              DateTime date = (data["date"] as Timestamp).toDate();
+
+              final Memo fetchMemo = Memo(
+                id: docs[index].id,
+                title: data["title"],
+                date: date,
+                participants: List<String>.from(data["participants"]),
+              );
+
+              String formattedDate =
+                  DateFormat('yyyy年MM月dd日').format(fetchMemo.date);
+
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Card(
+                  elevation: 6,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: ListTile(
+                    title: Text(
+                      fetchMemo.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "日付: $formattedDate",
+                          style: TextStyle(color: Colors.grey[600]),
+                        ),
+                        Text(
+                          "参加者: ${fetchMemo.participants.length}人",
+                          style: TextStyle(color: Colors.grey[600]),
+                        ),
+                      ],
+                    ),
+                    leading: const Icon(
+                      Icons.receipt_long,
+                      color: Colors.blueAccent,
+                      size: 40,
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.link),
+                          color: Colors.blueAccent,
+                          onPressed: () {
+                            _getLink(fetchMemo.id, widget.collectionName);
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete),
+                          color: Colors.redAccent,
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: const Text("確認"),
+                                  content: const Text("このメモを削除しますか？"),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text("キャンセル"),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        _deleteMemo(fetchMemo.id);
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text("削除"),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.grey[400],
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MemoDetailPage(
+                            collectionName: widget.collectionName,
+                            memoId: docs[index].id,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            FloatingActionButton.extended(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const CollectionInputPage(),
+                  ),
+                );
+              },
+              backgroundColor: const Color(0xFF75A9D6),
+              foregroundColor: Colors.white,
+              label: const Text("グループ選択"),
+              icon: const Icon(Icons.folder_open),
+            ),
+            FloatingActionButton.extended(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddMemoPage(
+                      collectionName: widget.collectionName,
+                    ),
+                  ),
+                );
+              },
+              backgroundColor: const Color(0xFF75A9D6),
+              foregroundColor: Colors.white,
+              label: const Text("メモ追加"),
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: const Color(0xFFE0ECF8), // 背景色,
+    );
+  }
+}
